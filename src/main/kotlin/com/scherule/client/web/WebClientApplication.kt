1902ad1bde03f7150c30d2db5fce536db2e67cf7
Grzegorz Gurgul <gurgul.grzegorz@gmail.com>
2018-01-05 16:38:37 +0100

    cleanup

--- src/main/kotlin/com/scherule/client/web/WebClientApplication.kt
+++ src/main/kotlin/com/scherule/client/web/WebClientApplication.kt
@@ -1,12 +1,10 @@
 package com.scherule.client.web
 
 import org.slf4j.LoggerFactory
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
-import org.springframework.cloud.netflix.zuul.filters.RouteLocator
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
